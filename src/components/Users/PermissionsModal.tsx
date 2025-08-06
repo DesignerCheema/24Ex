bfd8e40a06af1cdcc76dd35db450172b2601c75b
DesignerCheema <127754238+DesignerCheema@users.noreply.github.com>
2025-08-06 16:43:15 +0500

    Fix missing useEffect import

--- src/components/Users/PermissionsModal.tsx
+++ src/components/Users/PermissionsModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { XMarkIcon, ShieldCheckIcon } from '@heroicons/react/24/outline';
 import { User, Permission } from '../../types';
 
